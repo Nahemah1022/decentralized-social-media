32d711f833bf74370d7a985d146d4254cdd5d062
lastwhisper <lastwhisper98@gmail.com>
2024-04-18 20:52:34 -0500

    Init backend

--- backend/app/app.py
+++ backend/app/app.py
@@ -3,20 +3,24 @@ from flask import Flask, send_from_directory, request
 app = Flask(__name__, static_folder='../../frontend/dist', static_url_path='')
 
 
-@app.route('/api/<path:subpath>')
-def api(subpath):
-    # Your API handling logic here
-    return {"message": "This is an API endpoint"}
-
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    if request.path.startswith('/api/'):
-        # Allow the routing for API to naturally return Not Found if no endpoint matches.
-        pass
+@app.route('/api/test')
+def test():
+    name = request.args.get('name', None)  # Get 'name' from query string, default to None if not provided
+    if name:
+        return f"Hello, {name}!"
     else:
-        return send_from_directory(app.static_folder, 'index.html')
+        return "Hello, World!"
+
+
+@app.route('/api/<path:path>')
+def api(path: str):
+    return {"message": f"fetching for {path}..."}
+
+
+@app.route('/')
+@app.route('/about')
+def home():
+    return send_from_directory(app.static_folder, 'index.html')
 
 
 if __name__ == "__main__":
